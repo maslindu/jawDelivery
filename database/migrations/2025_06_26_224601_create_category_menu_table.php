3bf16f2646de868d2bbe16471f71463a008c6ac5
NabilRZ <nabilriano@gmail.com>
2025-06-26 23:40:25 +0700

    fixed crud category and menu

--- database/migrations/2025_06_26_224601_create_category_menu_table.php
+++ database/migrations/2025_06_26_224601_create_category_menu_table.php
@@ -4,15 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     public function up(): void
     {
         if (!Schema::hasTable('category_menu')) {
             Schema::create('category_menu', function (Blueprint $table) {
                 $table->id();
-                $table->unsignedBigInteger('category_id');
-                $table->unsignedBigInteger('menu_id');
+                $table->foreignId('category_id')->constrained()->onDelete('cascade');
+                $table->foreignId('menu_id')->constrained()->onDelete('cascade');
                 $table->timestamps();
+                
+                $table->unique(['category_id', 'menu_id']);
             });
         }
     }
